3413a1409c5c4ac28337aa8fdfda46ddf9fe7419
OrHell <43348516+OrHell@users.noreply.github.com>
2020-02-03 17:36:53 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 from PIL import Image
-import numpy as np
-from matplotlib import pyplot
+
+def toFixed(numObj, digits=0):
+    return f"{numObj:.{digits}f}"
 i=0
 j=0
+
 im = Image.open('test1.png')
 im2 = Image.open('test2.png')
+
 size = im.size
+
 count_error = 0
 count_access = 0
 all_count = 0
+
 width = size[0]
 height = size[1]
-def toFixed(numObj, digits=0):
-    return f"{numObj:.{digits}f}"
+
 for j in range (0, width):
     for i in range(0,height):
         first_image= im.getpixel((j,i))
@@ -33,30 +37,20 @@ print ("Совпадений: "+str(count_access) + "%")
 print ("Не совпадений: "+str(count_error)+ "%")
 print ("Всего сравнений:", all_count)
 
+# Функция записи изображения в файл
 
+def image_writer(file_name,w,h,image,opened_image):
+    file = open(file_name, 'w')
+    for x in range(0,w):
+        for y in range(0,h):
+            opened_image = image.getpixel((x,y))
+            fr = str(opened_image)
+            file.write(fr+'\n')
+    file.close()
+image_writer("file_image.txt",width,height,im,first_image)
+image_writer("file_image_second.txt", width, height,im2, second_image)
 
-file = open("file_image.txt", 'w')
-
-for j in range (0, width):
-    for i in range(0,height):
-        first_image= im.getpixel((j,i))
-        fr = str(first_image)
-        file.write(fr+'\n')
-cur = 0       
-file.close()
-
-
-
-file = open("file_image_second.txt", 'w')
-
-for j in range (0, width):
-    for i in range(0,height):
-        second_image= im2.getpixel((j,i))
-        fr = str(second_image)
-        file.write(fr+'\n')
-cur = 0       
-file.close()
-
+# Чтение изображения из файла
 with open ("file_image.txt",'r') as file_image:
     image_array = [row.strip() for row in file_image]
 with open ("file_image_second.txt",'r') as file_image:
